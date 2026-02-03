e0e1f4d310b1aa1bd59e65164f06dfc41fe79b38
Jchooper725 <jackson.hooper@maine.edu>
2026-02-03 18:56:26 -0500

    solved-branch-css commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
+            <h1>This is heading text</h1>
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Jackson Hooper
+                automatically reload.
+                <span style={{ color: "red" }}>Jackson Hooper</span>
             </p>
+            <ul>
+                <li>First</li>
+                <li>Second</li>
+                <li>Third</li>
+            </ul>
+            <img
+                src="../Users/jacksonhooper/Downloads/grammer_flyer.png"
+                alt="a flyer about apostrophies"
+            />
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col style={{ backgroundColor: "red" }}>First Column</Col>
+                    <Col>Second Column</Col>
+                </Row>
+            </Container>
         </div>
     );
 }
